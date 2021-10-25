53970206989ad6a432ab12697e1785ef95bb9e2c
somon sikder <somonsikder777@gmail.com>
2021-10-26 02:22:35 +0600

    last commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config({path: './.env'})
 
 const app = express()
 app.use(cors())
